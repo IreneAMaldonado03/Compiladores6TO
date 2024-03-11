d28d3375e8c66186ddd7a8ef302ecbb06d6d1bb9
IreneAMaldonado03 <irenemaldonado319@aragon.unam.mx>
2024-03-10 21:17:39 -0600

    Programa funcional, pero aun sin llegar al potencial esperado

--- parUno_imparCero/src/fes/aragon/parImpar.java
+++ parUno_imparCero/src/fes/aragon/parImpar.java
@@ -8,65 +8,87 @@ import java.util.List;
 
 public class parImpar {
     public static void main(String[] args) {
-    String archivo = "fuente.txt";
-    List<String> lineas = new ArrayList<>();
-
-    try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
-        String linea;
-        while ((linea = br.readLine()) != null) {
-            lineas.add(linea);
-        }
-    } catch (IOException e) {
-        System.out.println("Error al leer el archivo: " + e.getMessage());
-        System.exit(1);
+        String archivo = "fuente.txt";
+        validarCadenas(archivo);
     }
 
-    for (int i = 0; i < lineas.size(); i++) {
-        String linea = lineas.get(i);
-        boolean paresValidos = true;
-        boolean imparesValidos = true;
+    public static void validarCadenas(String archivo) {
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                validarCadena(linea);
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        }
+    }
 
-        for (int j = 0; j < linea.length(); j++) {
-            char caracter = linea.charAt(j);
+    public static void validarCadena(String cadena) {
+        int indice = 0;
+        int estadoActual = 0;
+        int contadorUnos = 0;
+        int contadorCeros = 0;
 
-            if (caracter != '0' && caracter != '1') {
-                System.out.println(linea + " invalida: Caracter invalido detectado, el programa solo acepta 0 y 1");
-                paresValidos = false;
-                imparesValidos = false;
-                break;
+        while (indice <= cadena.length() - 1) {
+            char c = cadena.charAt(indice);
+
+            if (c != '0' && c != '1') {
+                System.out.println("Caracter o caracteres invalidos detectados, el programa solo acepta 0 y 1");
+                return;
             }
 
-            if (caracter == '1') {
-                int contadorPares = 0;
-                int contadorImpares = 0;
-
-                for (int k = j; k < linea.length(); k++) {
-                    if (linea.charAt(k) == '1') {
-                        contadorPares++;
-                    } else {
-                        contadorImpares++;
+            switch (estadoActual) {
+                case 0:
+                    if (c == '0') {
+                        estadoActual = 1;
+                        contadorCeros++;
+                    } else if (c == '1') {
+                        estadoActual = 2;
+                        contadorUnos++;
                     }
-                }
-
-                if (contadorPares % 2 != 0) {
-                    paresValidos = false;
-                }
-
-                if (contadorImpares % 2 == 0) {
-                    imparesValidos = false;
-                }
+                    break;
+                case 1:
+                    if (c == '0') {
+                        estadoActual = 1;
+                        contadorCeros++;
+                    } else if (c == '1') {
+                        estadoActual = 2;
+                        contadorUnos++;
+                    }
+                    break;
+                case 2:
+                    if (c == '0') {
+                        estadoActual = 1;
+                        contadorCeros++;
+                    } else if (c == '1') {
+                        estadoActual = 3;
+                        contadorUnos++;
+                    }
+                    break;
+                case 3:
+                    if (c == '0') {
+                        estadoActual = 1;
+                        contadorCeros++;
+                    } else if (c == '1') {
+                        estadoActual = 3;
+                        contadorUnos++;
+                    }
+                    break;
             }
+            indice++;
         }
 
-        if (paresValidos && imparesValidos) {
-            System.out.println(linea + " valida para pares de uno, valida para impares de cero");
-        } else if (!paresValidos && imparesValidos) {
-            System.out.println(linea + " invalida para pares de uno, valida para impares de cero");
-        } else if (paresValidos && !imparesValidos) {
-            System.out.println(linea + " valida para pares de uno, invalida para impares de cero");
+        if (contadorUnos % 2 == 0) {
+            System.out.println(cadena + " valida para pares de uno");
         } else {
-            System.out.println(linea + " invalida para pares de uno, invalida para impares de cero");
+            System.out.println(cadena + " invalida para pares de uno");
+        }
+
+        if (contadorCeros % 2 != 0) {
+            System.out.println(cadena + " valida para impares de cero");
+        } else {
+            System.out.println(cadena + " invalida para impares de cero");
         }
     }
 }
-}
+
